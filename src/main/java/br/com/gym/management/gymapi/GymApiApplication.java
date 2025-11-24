dafbda2aa2ffd05a7a9dbde33134c2e2efab6168
José Luiz <jluizbc94@gmail.com>
2025-11-24 16:46:49 -0300

    fix(ci): Forçando novo push para disparar o workflow de Actions

--- src/main/java/br/com/gym/management/gymapi/GymApiApplication.java
+++ src/main/java/br/com/gym/management/gymapi/GymApiApplication.java
@@ -1,7 +1,4 @@
 package br.com.gym.management.gymapi;
-// Comentário para acionar o CI/CD
-// Comentário para acionar o CI/CD// Comentário para acionar o CI/CD
-
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
